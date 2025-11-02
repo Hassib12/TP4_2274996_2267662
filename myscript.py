15b432dabca6f84acd28fcaa8a638c41efcd2a47
Axo47 <ezzedinehassibkevin@gmail.com>
2025-11-02 16:19:41 -0500

    FORGOT THE SUSSY GUY

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import os
+
+GOOD = os.environ.get("GOOD_HASH")
+BAD = os.environ.get("BAD_HASH")
+
+os.system(f"git bisect start {BAD} {GOOD}")
+os.system("git bisect run .venv/bin/python manage.py test")
+
